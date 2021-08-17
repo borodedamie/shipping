3719b2f8c07e953a81e6c36ff8420648932b879b
borodedamie <opeoluborode@yahoo.com>
2021-08-17 06:22:44 -0700

    app made available for changes

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import model
+import os
+
+UPLOAD_FOLDER = '/mnt/c/Users/PC/Desktop/Shipping/static/images/upload'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           
+@app.route('/submit', methods = ['GET', 'POST'])
+def submit_application():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        other_names = request.form['other_names']
+        email = request.form['email']
+        address = request.form['address']
+        state = request.form['state']
+        city = request.form['city']
+        phone_no = request.form['phone_no']
+        ssn = request.form['ssn']
+        position = request.form['position']
+        reference = request.form['reference']
+        if request.files:
+            
+            files = request.files['files']
+            files.save(os.path.join(app.config['UPLOAD_FOLDER'], files.filename))
+            
+        # model.save_application_to_db(first_name, last_name, other_names, email, address, state, city, phone_no, ssn, position, files, reference)
+        
+        return 'something happened at least.'
+
+@app.route('/')
+def load_homepage():
+    return render_template("index.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/logistics')
+def logistics():
+    return render_template("logistics.html")
+
+@app.route('/services')
+def services():
+    return render_template("services.html")
+
+@app.route('/careers')
+def careers():
+    return render_template("career/careers.html")
+
+@app.route('/careers2')
+def careers_contd():
+    return render_template("career/careers2.html")
+
+@app.route('/application')
+def application():
+    return render_template("career/application.html")
+
+@app.route('/create', methods = ['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone_no = request.form['phone_no']
+        message = request.form['message']
+        
+        model.save_contact_to_db(name, email, phone_no, message)
+        
+        return redirect(url_for('contact'))
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port = 8000, debug = True)
---
+++ schema.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+connection = sqlite3.connect('shipbite.db', check_same_thread = False)
+cursor = connection.cursor()
+
+cursor.execute(
+    """ CREATE TABLE IF NOT EXISTS contacts (
+        id INTEGER PRIMARY KEY,
+        name text,
+        email text,
+        phone_no text,
+        message text
+        ); """
+    )
+
+cursor.execute(
+    """ CREATE TABLE IF NOT EXISTS applications (
+        id INTEGER PRIMARY KEY,
+        first_name text,
+        last_name text,
+        other_names text NULL,
+        email text,
+        address text,
+        state text,
+        city text,
+        phone_no text,
+        ssn text,
+        position text,
+        files blob,
+        reference text 
+    ); """
+)
+
+print("Tables created successfully")
+
+connection.close()
